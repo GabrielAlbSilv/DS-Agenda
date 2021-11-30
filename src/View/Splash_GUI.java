b2eaa1e6c7c4e2df6d2a86009505ce5bfc86aaf9
GabrielAlbSilv <gabrielalbuquerquemoura@gmail.com>
2021-11-30 19:47:25 -0300

    first commit

---
+++ src/View/Splash_GUI.java
@@ -0,0 +1,142 @@
+
+package View;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Splash_GUI extends javax.swing.JFrame {
+
+    public Splash_GUI() {
+        initComponents();
+        
+        //----------------------Inicio----------------------------
+                new Thread(){
+        
+            public void run() {
+        
+        
+        for (int i=0; i<101; i++){
+                    try {
+                        sleep(60); 
+                        
+                        progresso.setValue(i);
+                        
+                        if(progresso.getValue() == 10){
+                        
+                            mensagem.setText("Fazendo a conexao com o banco de dados");
+                            sleep(2000);
+                                                       
+                        
+                        }else if (progresso.getValue() <=30){
+                            mensagem.setText("Carregando o sistema");
+                          sleep(100);  
+                        } else if (progresso.getValue() <=99){
+                            mensagem.setText("Carregamento quase completo");
+                            
+                        }else{
+                            mensagem.setText("Carregamento completo. Seu programa sera iniciado.");
+                        
+                        sleep(3000);
+                        
+                       new Login_GUI().setVisible(true);
+			dispose();
+                        }
+                        
+                        
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Splash_GUI.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+        
+        
+        
+        
+        }        
+            }
+        
+        }.start();
+        
+    
+        
+        
+        
+        //----------------------Termino-------------------------
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        progresso = new javax.swing.JProgressBar();
+        mensagem = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        jPanel1.setLayout(null);
+        jPanel1.add(progresso);
+        progresso.setBounds(76, 204, 250, 20);
+        jPanel1.add(mensagem);
+        mensagem.setBounds(80, 240, 240, 20);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        setSize(new java.awt.Dimension(400, 300));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Splash_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Splash_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Splash_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Splash_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Splash_GUI().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel mensagem;
+    private javax.swing.JProgressBar progresso;
+    // End of variables declaration//GEN-END:variables
+}
